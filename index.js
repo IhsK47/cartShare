4a50ec6bee9dd78580f91e0265ae59e083422597
Ihsan Kabir <i.kabir1210@gmail.com>
2026-08-10 13:08:48 +0100

    prisma schema edited

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-'use strict';
+"use strict";
 
-const Hapi = require('@hapi/hapi');
+const Hapi = require("@hapi/hapi");
 
 const init = async () => {
+  
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost', //probably gonna changed to be www.cartshare.com
-    });
+    port: 3000,
+    host: "localhost", //probably gonna changed to be www.cartshare.com
+  });
 
-    await server.route({ //get method
-        method: 'GET',
-        path: '/health',
-        handler: (request, h) => { //req is incoming, h is output
-                        return {
-                status: "OK",
-                timestamp: new Date().toISOString(),
-                message: "cartshare backend is running"
-            };
-        },
-    });
+  await server.route({
+    //get method
+    method: "GET",
+    path: "/health",
+    handler: (request, h) => {
+      //req is incoming, h is output
+      return {
+        status: "OK",
+        timestamp: new Date().toISOString(),
+        message: "cartshare backend is running",
+      };
+    },
+  });
 
     await server.route({
-        method: "POST",
-        path: "/example",
-        handler: (request, reply) => {
-
-            // const wtv = request.payload.fieldname
-            //map
-            //service layer i.e. prisma logic
-
-            return reply.response({
-                message: ""
-            }).code(201)
+    //get method
+    method: "GET",
+    path: "/hello/{name}",
+    handler: (request, h) => {
+      return {
+        message: "hello,"+ request.params.name,
+      };
+    },
+  });
 
 
-        }
-    })
 
+  await server.route({
+    method: "POST",
+    path: "/example",
+    handler: (request, reply) => {
+      // const wtv = request.payload.fieldname
+      //map potentially
+      //service layer i.e. prisma logic
 
-    await server.start(); //wakeup hapi.js and get
-    console.log('Server running on %s', server.info.uri);
+      return reply
+        .response({
+          message: "",
+        })
+        .code(201);
+    },
+  });
+
+  await server.start(); //wakeup hapi.js and get
+  console.log("Server running on %s", server.info.uri);
 };
 
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
